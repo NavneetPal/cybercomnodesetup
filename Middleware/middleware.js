1162e91f0eb3ac4cfb5e5447829ff1cf149a6cde
Navneet <navneetpal8106@gmail.com>
2021-04-01 19:04:36 +0530

    new separate folder createdfor the node setup

---
+++ Middleware/middleware.js
@@ -0,0 +1,17 @@
+const expressJwt=require('express-jwt');
+
+module.exports={
+    isSignedIn:expressJwt({
+        secret:process.env.SECRET,
+        algorithms:["HS256"],
+    }),
+    isAdmin:(req,res,next)=>{
+        if(req.user.role==='0'){
+            return res.json({
+                error:"You are not admin"
+            })
+        }else{
+            next();
+        }
+    }
+}
---
+++ db/migrations/20210302143445-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(50),
+        allowNull:false
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull:false
+      },
+      password: {
+        type: Sequelize.STRING(250),
+        allowNull:false
+      },
+      role: {
+        type: Sequelize.ENUM('0','1'),
+        defaultValue:'0'
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
